daab9811ff1212cffd238de6706aaf4c74d86f01
mahmoudalsayed123 <mahmoudalsayed05789@gmail.com>
2026-07-31 14:47:48 +0300

    add new epic

--- app/(main)/project/[projectId]/epics/new/page.tsx
+++ app/(main)/project/[projectId]/epics/new/page.tsx
@@ -1,8 +1,33 @@
+import MainHeading from "@/components/layout/MainHeading";
+import FormNewEpic from "@/features/epic/components/FormNewEpic";
 
-const NewEpicPage = () => {
+const NewEpicPage = async ({
+  params,
+}: {
+  params: Promise<{ projectId: string }>;
+}) => {
+  const { projectId } = await params;
   return (
-    <div>NewEpicPage</div>
-  )
-}
+    <section className="pt-8 px-6 pb-13 lg:p-2">
+      {/* main heading for desktop */}
+      <div className="mb-6 w-85.5 md:w-lg lg:mb-10">
+        <MainHeading
+          heading="Create New Epic"
+          title="Define a major project phase or high-level milestone to group
+          related tasks and track architectural progress."
+          sizeDesktop="no-project"
+          sizeMobile="heading-MD"
+        />
+      </div>
 
-export default NewEpicPage
+      {/* form */}
+      <section className="lg:w-full lg:flex lg:items-center lg:justify-center lg:p-8">
+        <div className="lg:bg-white lg:rounded-lg lg:w-213 lg:max-w-213 shadow-form">
+          <FormNewEpic projectId={projectId} />
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default NewEpicPage;

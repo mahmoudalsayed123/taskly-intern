7373852a5ead319363660b977357db26cf732aed
mahmoudalsayed123 <mahmoudalsayed05789@gmail.com>
2026-08-26 17:23:34 +0300

    refactor search epic component make the code are simple

--- features/epic/components/SearchEpic.tsx
+++ features/epic/components/SearchEpic.tsx
@@ -4,34 +4,26 @@ import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-const SearchEpic = ({
-  projectId,
-}: {
-  projectId: string;
-}) => {
+const SearchEpic = ({ projectId }: { projectId: string }) => {
   const searchParams = useSearchParams();
 
   const [search, setSearch] = useState(searchParams.get("title") || "");
-  const [debouncedSearch, setDebouncedSearch] = useState("");
   const router = useRouter();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setDebouncedSearch(search);
+      const params = new URLSearchParams(searchParams.toString());
+
+      if (search) {
+        params.set("title", search.replace(" ", ""));
+      } else {
+        params.delete("title");
+      }
+      router.push(`/project/${projectId}/epics?${params.toString()}`);
     }, 300);
     return () => clearTimeout(timer);
   }, [search]);
 
-  useEffect(() => {
-    const params = new URLSearchParams(searchParams.toString());
-
-    if (debouncedSearch) {
-      params.set("title", debouncedSearch.replace(" ", ""));
-    } else {
-      params.delete("title");
-    }
-    router.push(`/project/${projectId}/epics?${params.toString()}`);
-  }, [debouncedSearch]);
   return (
     <div className="relative">
       <input
@@ -40,7 +32,7 @@ const SearchEpic = ({
         }}
         type="text"
         name="title"
-        value={debouncedSearch}
+        value={search}
         placeholder="Search for epics..."
         className="input lg:m-0! w-full! lg:w-75.75! h-12! rounded-xs! py-1.5! px-3! ps-7! placeholder:text-body-MD placeholder:font-normal placeholder:text-resend-timer"
       />
